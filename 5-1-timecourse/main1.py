7414302f32b405f9afc5ecc0ca3d1d2785033dec
Chris Rodgers <xrodgers@gmail.com>
2014-04-27 21:27:49 -0400

    removing unnecessary event from timecourse locking

--- 5-1-timecourse/main1.py
+++ 5-1-timecourse/main1.py
@@ -20,90 +20,7 @@ else:
     cached = {}
 
 
-def cached_load_prev_rew_stop(ulabel, trials_info):
-    """Caches prev_rew_stop for session; otherwise loads it from bcontrol
-    
-    ulabel : uses this to figure out which session, and checks the cache
-    trials_info : uses this info, especially after_go_hit, to find
-        prev_rew_stop
-    
-    Returns: series prev_rew_stop
-        You can insert this into trials_info
-    """
-    # which session?
-    session = kkpandas.kkrs.ulabel2session_name(ulabel)
-    
-    # return immediately if already in
-    if session in cached:
-        return cached[session]
-    
-    # get peh
-    rs = my.dataload.session2rs(session)
-    bcf_l = glob.glob(os.path.join(rs.full_path, '*.mat'))
-    assert len(bcf_l) == 1
-    bcl = bcontrol.Bcontrol_Loader(filename=bcf_l[0])
-    bcl.load()
-    bcld = bcontrol.dictify_mat_struct(bcl.data)
-    peh = bcld['peh']
-    
-    # get sync
-    b2n_sync = np.loadtxt(os.path.join(rs.full_path, 'SYNC_B2N'))   
 
-    # get time of previous reward
-    prev_rew_ser = get_prev_rew_stop_from_peh(peh, trials_info, b2n_sync)
-    
-    # put in cache
-    cached[session] = prev_rew_ser
-    
-    # return
-    return prev_rew_ser
-
-def get_prev_rew_stop_from_peh(peh, trials_info, b2n_sync):
-    """Go through peh and trials_info, return time of previous reward
-    
-    trials_info : after_go_hit must be set
-    """
-    # If previous trial was GO hit, get last outpoke on this trial
-    rewlast_d = {}
-    for ti_num in my.pick(trials_info, after_go_hit=1):
-        # ti_num is the btrial number which starts at 1
-        # we subtract one and index into peh
-        # in certain circumstances I think TI can have one more than peh
-        pokes_trial = peh[ti_num - 1]['pokes']
-    
-        # check the port that was rewarded on the previous trial
-        if ti_num - 1 not in trials_info.index:
-            # This can happen if previous trial was munged
-            continue
-        port = trials_info['correct_side'][ti_num - 1][0].upper()
-        
-        # Get the pokes from the current trial
-        if pokes_trial[port].size == 0:
-            # no pokes
-            continue
-        if pokes_trial[port].ndim == 1:
-            # overflattened
-            outpokes = np.array([pokes_trial[port][1]])
-        else:
-            outpokes = pokes_trial[port][:, 1]
-        outpokes = outpokes[~np.isnan(outpokes)]
-        
-        # convert to neural time base
-        outpokes_nbase = np.polyval(b2n_sync, outpokes)
-        
-        # get the last outpoke before center center poke
-        outpokes_nbase = outpokes_nbase[
-            outpokes_nbase < trials_info['cpoke_start'][ti_num]]
-        if len(outpokes_nbase) == 0:
-            continue
-        else:
-            rewlast_d[ti_num] = outpokes_nbase.max()
-    res = pandas.Series(rewlast_d)
-    
-    # Check that prev_rew_stop was always after previous choice_made
-    assert np.all(res > trials_info['choice_made'].shift(1)[res.index])
-    
-    return res
 
 
 def insert_events_and_times(ulabel, trials_info):
@@ -130,10 +47,6 @@ def insert_events_and_times(ulabel, trials_info):
     trials_info['prev_nogo_stop'][
         trials_info.after_nogo_hit != 1] = np.nan
     
-    # If previous trial was GO hit, get previous reward stop
-    trials_info['prev_rew_stop'] = cached_load_prev_rew_stop(
-        ulabel, trials_info)
-    
     return trials_info
 
 
@@ -149,7 +62,7 @@ base_tpk = {
 # split across each category of trial
 event_name_list = [
     'cpoke_start', 'stim_onset', 'cpoke_stop', 'choice_made', 'next_start',
-    'prev_rew_stop', 'prev_nogo_stop']
+    'prev_nogo_stop']
 
 # Various trial splits
 after_trial_type_l = ['after_nogo_hit', 'after_go_hit', 'after_all']
